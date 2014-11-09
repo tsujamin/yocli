52c99b0f1db3d669a1fda6403b3958e0a92e31e2
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2014-11-09 12:26:32 +1100

    Initial Commit

---
+++ yocli/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  yocli
+//
+//  Created by Benjamin Roberts on 9/11/2014.
+//  Copyright (c) 2014 tsu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
