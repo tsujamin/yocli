7fb4a91d9941c62674ddb3a3b5c33cec4d26e554
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2014-11-12 18:50:20 +1100

    added checks for username/queue in /yo/ added message to callback added errback

--- yosrv/yosrv.rb
+++ yosrv/yosrv.rb
@@ -9,10 +9,12 @@ message_queues = {}
 
 #get request for yo's
 get '/stream/:apikey', provides: 'text/event-stream' do
-  stream :keep_open do |stream| 
+  stream :keep_open do |stream|
     #extract key of request
     key = params[:apikey]
     
+    puts "new connection (#{key})"
+
     #remove/close old connection if exists
     if connections.include? key
       connections[key].close
@@ -23,33 +25,55 @@ get '/stream/:apikey', provides: 'text/event-stream' do
     connections[params[:apikey]] = stream
     
     #delete if stream closes
-    stream.callback { connections.delete(key) } 
+    stream.callback { 
+      puts "stream closed (#{key})"
+      connections.delete(key) 
+    }
+
+    stream.errback {
+      puts "stream errored (#{key})"
+      connections.delete(key)
+    }
     
     #create the queue
     message_queues[key] ||= [] 
     
     #push queued messages out
-    #TODO
+    message_queues[key].drop_while {|yo| stream << yo}
   end
 end
 
 #yoapi callback
 get '/yo/:apikey' do
-  #extract relevant dict fields  
-  yo = params.select {|key,_| ['username'].include? key}
-  yo = params[:timestamp] = Time.now
   key = params[:apikey]
+  
+  #check for existing queue
+  unless message_queues.include? key
+    puts "queue doesn't exist (#{key})"
+    halt 400
+  end
+
+  #check for valid request:
+  unless params.include? 'username'
+    puts "yo missing yosername (#{key})"
+    halt 400
+  end
+
+  #extract relevant dict fields  	
+  yo = params.select {|key,_| ['username'].include? key}
+  yo['timestamp'] = Time.now
   queue = message_queues[key]
-    
+
   if connections.include? key
     # send the received yo to the client
+    puts "forwarding yo from #{yo['username']} (#{key})"
     connection = connections[key]
     connection << yo.to_json
     
-  elsif message_queues.include? key
+  else
     # add the received yo to the cache queue
     # only for previously connected clients
-    queue = message_queues[key]
+    puts "queueing yo from #{yo['username']} (#{key})"
     
     queue << yo
     
